97851e2232a7cce0dfb1100a69eb5f30a87a6aef
wolfwar <wolfwar@outlook.com>
2020-03-30 22:01:19 +0200

    cleanup App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
 import { Navbar } from "./NavBar/Navbar";
 import { Banner } from "./Banner/Banner";
 import { Menu } from "./Menu/Menu";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Open Sans', sans-serif;
-    margin: 0;
-
-  }
-  h1,h2,h3 {
-    font-family: 'Righteous', cursive;
-  }
-`;
+import { GlobalStyle } from "./Styles/GlobalStyle";
 
 function App() {
   return (
@@ -22,7 +11,6 @@ function App() {
       <Navbar />
       <Banner />
       <Menu />
-      <div>ole</div>
     </>
   );
 }
--- src/NavBar/Navbar.js
+++ src/NavBar/Navbar.js
@@ -20,7 +20,12 @@ const Logo = styled(Title)`
 export function Navbar() {
   return (
     <NavbarStyled>
-      <Logo>Pizerria V 🍕</Logo>
+      <Logo>
+        Pizerria V{" "}
+        <span role="img" aria-label="pizza-slice">
+          🍕
+        </span>
+      </Logo>
     </NavbarStyled>
   );
 }
